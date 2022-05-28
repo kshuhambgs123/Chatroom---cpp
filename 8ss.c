5142ca859a01e57d4537460198e3763b42f94bde
Shubham Kumar <kshubhambgs123@gmail.com>
2022-05-28 12:05:01 +0530

    Add files via upload

---
+++ 8ss.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include<pthread.h>
+
+pthread_t send_m, rcv ;
+struct sockaddr_in server_addr, client_addr;
+struct sockaddr_in t_server_addr, t_client_addr;
+char server_message[2000], client_message[2000];
+pthread_t send_m, rcv ;
+int client_struct_length;
+int make;
+
+
+void *func1()
+{
+    
+    do{
+    
+    	
+    		//printf("Enter the message---");
+    		fgets( client_message, sizeof(client_message), stdin );
+    		if (sendto( make, client_message, strlen(client_message), 0, (struct sockaddr*)&client_addr, client_struct_length) < 0){
+        		printf("Can't send\n");
+        		return NULL;
+    		}
+    		memset(client_message,0,2000);
+    		
+    		//printf(" msg sent \n");
+    		
+    	
+    	
+    }while(1);
+    
+    
+    return NULL;
+}
+
+
+void *func2()
+{
+    
+    do{
+    
+    	
+    		if (recvfrom( make, client_message, sizeof(client_message), 0, (struct sockaddr*)&client_addr, &client_struct_length) < 0){
+        		printf("Couldn't receive\n");
+        		return NULL;
+   		}
+   		
+   		printf("Msg recieved ====>> %s\n", client_message);
+   		memset(client_message,0,2000);
+    		
+    	
+    	
+    }while(1);
+    
+    
+    return NULL;
+}
+
+
+int main(void){
+
+	client_struct_length = sizeof(client_addr);
+	
+	
+	make=socket( AF_INET, SOCK_DGRAM, 0 );
+	
+	
+	
+	/* OWN SOCKET */
+	server_addr.sin_family = AF_INET;
+    	server_addr.sin_port = htons(2000);
+    	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    	
+    	
+    	if(bind( make, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        	printf("Couldn't bind to the port\n");
+        	return -1;
+    	}
+    	
+    	
+    	
+    	/* SEND to RECIEVE from socket */
+    	client_addr.sin_family = AF_INET;
+    	client_addr.sin_port = htons(2001);
+    	client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    	
+    	
+    	
+    	pthread_create(&send_m, NULL, &func1, NULL);
+    	pthread_create(&rcv, NULL, &func2, NULL);
+    	
+    	pthread_join(send_m, NULL);
+ 	pthread_join(rcv,NULL);
+    	
+    	
+    	
+    	
+    	
+    	//while(1){
+    	
+    		/*if (recvfrom( make, client_message, sizeof(client_message), 0, (struct sockaddr*)&client_addr, &client_struct_length) < 0){
+        		printf("Couldn't receive\n");
+        		return -1;
+   		}
+   		
+   		printf("Msg from client: %s\n", client_message);
+   		
+   		
+   		strcpy( server_message, client_message );
+   		
+   		
+   		if (sendto( make, server_message, strlen(server_message), 0, (struct sockaddr*)&client_addr, client_struct_length) < 0){
+        		printf("Can't send\n");
+        		return -1;
+    		}
+    		
+    		printf(" msg sent \n");*/
+    		
+    		
+    		/*printf("Enter the message---");
+    		fgets( client_message, sizeof(client_message), stdin );
+    		if (sendto( make, client_message, strlen(client_message), 0, (struct sockaddr*)&client_addr, client_struct_length) < 0){
+        		printf("Can't send\n");
+        		return -1;
+    		}
+    		
+    		if (recvfrom( make, client_message, sizeof(client_message), 0, (struct sockaddr*)&client_addr, &client_struct_length) < 0){
+        		printf("Couldn't receive\n");
+        		return -1;
+   		}
+   		
+   		printf("Msg from client: %s\n", client_message);*/
+    		
+    		
+    	
+    	
+    	
+    	//}
+
+
+
+
+
+}
